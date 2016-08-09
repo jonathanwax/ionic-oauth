91b055503c146be4783f64c04120786123f05680
Jonathan Wax (Prodware) <jwax@prodware.fr>
2016-08-09 22:19:39 +0300

    implemented : https://www.sitepoint.com/authenticating-users-via-oauth-with-cordova-and-ionic/

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,28 +1,54 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('DashCtrl', function ($scope, $cordovaOauth, $http) {
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.googleLogin = function () {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+      // https://github.com/nraboy/ng-cordova-oauth
+      // $cordovaOauth.windowsLive(string clientId, array appScope, object options)
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+      $cordovaOauth.google("1076974280517-cl2s1abplmr3dhn9o85t50vuiaad5piq.apps.googleusercontent.com", ["email", "profile"]).then(function (result) {
+        $scope.showProfile = false;
+        $http.get("https://www.googleapis.com/plus/v1/people/me", { params: { access_token: result.access_token } })
+          .then(function (res) {
+
+            $scope.showProfile = true;
+            $scope.details = res.data;
+
+          }, function (error) {
+            alert("Error: " + error);
+          });
+
+      }, function (error) {
+        // error
+        $scope.details = 'got error';
+      });
+    }
+
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
